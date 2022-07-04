d4aed2b6eb7a4a3f3148bd6a78ac30e8774d7db5
spencer lebard <slebard@gmail.com>
2022-07-04 14:52:41 -0700

    getting records

--- force-app/main/default/lwc/assetComponent/assetComponent.js
+++ force-app/main/default/lwc/assetComponent/assetComponent.js
@@ -1,6 +1,34 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+
+import NAME_FIELD from '@salesforce/schema/Asset.Name';
+// import TITLE_FIELD from '@salesforce/schema/Asset.Title';
+// import PHONE_FIELD from '@salesforce/schema/Asset.Phone';
+// import EMAIL_FIELD from '@salesforce/schema/Asset.Email';
+// import PICTURE_FIELD from '@salesforce/schema/Asset.Picture__c';
+
+const fields = [
+    NAME_FIELD,
+    // TITLE_FIELD,
+    // PHONE_FIELD,
+
+];
 
 export default class AssetComponent extends LightningElement {
+    @api asset;
 
-    
+    @wire(getRecord, { recordId: '$recordId', fields }) asset;
+
+    get name() {
+        return getFieldValue(this.asset.data, NAME_FIELD);
+    }
+
+    // get title() {
+    //     return getFieldValue(this.asset.data, TITLE_FIELD);
+    // }
+
+    // get phone() {
+    //     return getFieldValue(this.asset.data, PHONE_FIELD);
+    // }
+
 }
