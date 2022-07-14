3cf247dafb1c7c3cc0317f5025c1e9eaf36f2c0e
spencer lebard <slebard@gmail.com>
2022-07-14 16:38:20 -0700

    jodans help code

--- force-app/main/default/lwc/assetComponent/assetComponent.js
+++ force-app/main/default/lwc/assetComponent/assetComponent.js
@@ -1,23 +1,33 @@
-import { LightningElement, wire, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import NAME_FIELD from '@salesforce/schema/Account.Name';
+import OWNER_EMAIL_FIELD from '@salesforce/schema/Account.Owner.Email';
 
-import NAME_FIELD from '@salesforce/schema/Asset.Name';
+export default class AccountGetRecord extends LightningElement {
+    /** Id of record to display. */
+    @api recordId;
 
-//NOTE need to create apex class that grabs assets and field names and then i can display
+    currentRecordId
 
-
-export default class AssetComponent extends LightningElement {
-    @api asset;
-
-    @wire(getRecord, { recordId: '$recordId', NAME_FIELD }) asset;
-
-    get assetName() {
-        return getFieldValue(this.asset.data, NAME_FIELD);
+    connectedCallback(){
+        console.log('hello')
     }
 
-        connectedCallback(){
-        console.log(this.AssetName())
+    handleIdChange(e){
+        this.currentRecordId = e.target.value;
     }
 
+    /* Expose schema objects/fields to the template. */
+    accountObject = ACCOUNT_OBJECT;
 
+    /* Load Account.Name for custom rendering */
+    @wire(getRecord, { recordId: '$currentRecordId', fields: [NAME_FIELD, OWNER_EMAIL_FIELD] })
+    record;
+
+    /** Get the Account.Name value. */
+    get nameValue() {
+        console.log('getting name: '+NAME_FIELD)
+        return this.record.data ? getFieldValue(this.record.data, 'Account.Name') : '';
+    }
 }
