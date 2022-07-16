d77e1798eda8c5635763a3da9290d81c18f9c50a
spencer lebard <slebard@gmail.com>
2022-07-16 16:18:22 -0700

    css

--- force-app/main/default/lwc/assetComponent/assetComponent.js
+++ force-app/main/default/lwc/assetComponent/assetComponent.js
@@ -1,10 +1,10 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
-import ACCOUNT_OBJECT from '@salesforce/schema/Account';
-import NAME_FIELD from '@salesforce/schema/Account.Name';
-import OWNER_EMAIL_FIELD from '@salesforce/schema/Account.Owner.Email';
+import ASS_OBJECT from '@salesforce/schema/Asset';
+import NAME_FIELD from '@salesforce/schema/Asset.Name';
+//import OWNER_EMAIL_FIELD from '@salesforce/schema/Account.Owner.Email';
 
-export default class AccountGetRecord extends LightningElement {
+export default class assetComponent extends LightningElement {
     /** Id of record to display. */
     @api recordId;
 
@@ -19,15 +19,17 @@ export default class AccountGetRecord extends LightningElement {
     }
 
     /* Expose schema objects/fields to the template. */
-    accountObject = ACCOUNT_OBJECT;
+    assObject = ASS_OBJECT;
 
-    /* Load Account.Name for custom rendering */
-    @wire(getRecord, { recordId: '$currentRecordId', fields: [NAME_FIELD, OWNER_EMAIL_FIELD] })
+    /* Load Asset.Name for custom rendering */
+    @wire(getRecord, { recordId: '$currentRecordId', fields: [NAME_FIELD,
+         //OWNER_EMAIL_FIELD
+        ] })
     record;
 
-    /** Get the Account.Name value. */
+    /** Get the Asset.Name value. */
     get nameValue() {
-        console.log('getting name: '+NAME_FIELD)
-        return this.record.data ? getFieldValue(this.record.data, 'Account.Name') : '';
+        console.log('getting name: '+ NAME_FIELD)
+        return this.record.data ? getFieldValue(this.record.data, 'Asset.Name') : '';
     }
 }
