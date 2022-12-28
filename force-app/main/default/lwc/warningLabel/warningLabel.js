2d7f765ac500da09d833a73988d88fa1c0fece6e
spencer lebard <slebard@gmail.com>
2022-12-28 10:16:56 -0800

    push

---
+++ force-app/main/default/lwc/warningLabel/warningLabel.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class WarningLabel extends LightningElement {
+    showifButtonClicked(){
+        this.showifButtonClicked = true;
+      }
+
+}
