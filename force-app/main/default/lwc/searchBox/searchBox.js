bfaab55977e7cfe01bde8ad9295d89dfe850211c
spencer lebard <slebard@gmail.com>
2023-01-11 16:38:45 -0800

    pull down and push up contacts search

--- force-app/main/default/lwc/searchBox/searchBox.js
+++ force-app/main/default/lwc/searchBox/searchBox.js
@@ -1,6 +1,18 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import contactQuery from '@salesforce/apex/QueryContacts.contactQuery';
 
 export default class SearchBox extends LightningElement {
 
-    
+    @track contactList
+    @track contact
+
+    @wire(contactQuery)
+    getContact({data, error}){
+        if(data){
+            this.accountList = data
+        }else if (error){
+            this.error = error
+        }
+   
+    }
 }
--- force-app/main/default/triggers/InteractionTrigger.trigger
+++ force-app/main/default/triggers/InteractionTrigger.trigger
@@ -1,4 +1,4 @@
-trigger InteractionTrigger on Interaction__c (before update) {
+trigger InteractionTrigger on Interaction__c (before insert, before update) {
     if(Trigger.isBefore && Trigger.isUpdate){
         InteractionTriggerHandler.isBeforeUpdate(Trigger.new);
     }
