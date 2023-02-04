11480d9ec74b65ee9939fe4582a7da5979ceab5f
SpencerLeBard <slebard@gmail.com>
2023-02-03 17:09:27 -0700

    data table filling with contacts

--- force-app/main/default/lwc/searchBox/searchBox.js
+++ force-app/main/default/lwc/searchBox/searchBox.js
@@ -2,14 +2,16 @@ import { LightningElement, track, wire } from 'lwc';
 import contactQuery from '@salesforce/apex/QueryContacts.contactQuery';
 
 const columns = [
-    { label: 'Id' , fieldName: 'Id'} ,
-    { label: 'First Name' , fieldName: 'FirstName'} ,
-    { label: 'Last Name' , fieldName: 'LastName'} ,
-]; 
+    { label: 'First Name', fieldName: 'FirstName' },
+    { label: 'Last Name', fieldName: 'LastName' },
+    { label: 'Title', fieldName: 'Title' },
+    { label: 'Phone', fieldName: 'Phone', type: 'phone' },
+    { label: 'Email', fieldName: 'Email', type: 'email' },
+];
 export default class SearchBox extends LightningElement {
 
 
-    @track contactList;
+    @track contactsList;
     @track contact;
     @track data = [];
     @track columns = columns;
@@ -22,16 +24,17 @@ export default class SearchBox extends LightningElement {
     handleClick(event) {
         this.clickedButtonLabel = event.target.label;
         console.log('click in search box')
+        
     }
 
     // TODO : Apparently this wire method fires as it renders like a connected callback
     // but does not do anything when i click the button :(
     @wire(contactQuery , {contactInput: '$param'})
     getContact({data, error}){
-        console.log('hit getcontact method')
+        // console.log('hit getcontact method')
         if(data){
-            this.contactList = data.data
-            console.log('has data')
+            this.contactsList = data
+            // console.log('has data')
         }else if (error){
             this.error = error
             console.error(this.error)
