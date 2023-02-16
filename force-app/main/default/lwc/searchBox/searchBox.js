61ac35f6f75dec4dac8609b8cb03a8cb34aa68e9
SpencerLeBard <slebard@gmail.com>
2023-02-16 11:04:29 -0700

    working!!!

--- force-app/main/default/lwc/searchBox/searchBox.js
+++ force-app/main/default/lwc/searchBox/searchBox.js
@@ -11,27 +11,19 @@ const columns = [
 export default class SearchBox extends LightningElement {
 
 
-    @track contactsList = [];
-    @track contact;
-    @track columns = columns;
     @track contactInput = '';
+    @track contacts;
+
     
 
     @wire(contactQuery , {contactInput: '$contactInput'})
-    getContacts({data, error}){
-        if(data){
-            console.log(data.data)
-        } else if (error){
-            console.log(error)
-        }
-    }
+        contacts;
   
         handleTyping(event) {
             window.clearTimeout(this.delayTimeout);
             const contactInput = event.target.value;
             this.delayTimeout = setTimeout(() => {
                 this.contactInput = contactInput;
-                console.log('contactInput: '+ contactInput)
                 this.getContacts(contactInput)
             }, 300);
     }
