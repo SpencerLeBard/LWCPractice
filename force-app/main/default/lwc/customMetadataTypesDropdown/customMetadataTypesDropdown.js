b8f6980fa21e502af22e0fdac48ac4e44fd3c74a
SpencerLeBard <slebard@gmail.com>
2023-04-13 09:43:36 -0600

    working!!

--- force-app/main/default/lwc/customMetadataTypesDropdown/customMetadataTypesDropdown.js
+++ force-app/main/default/lwc/customMetadataTypesDropdown/customMetadataTypesDropdown.js
@@ -11,19 +11,31 @@ getFavPlayer({ data, error }) {
   if (data) {
     for(let i = 0; i <= 1; i++){
       let player = data[i];
-      console.log({label: player, value: `${player + i}`})
+      // console.log({label: player, value: `${player + i}`})
       this.favPlayers.push({label: player, value: `${player + i}`});
-      console.log(this.favPlayers);
+
+      //NOTE this.favPlayer is returning a proxy object and i can't figure out how to drill into the data
+
+      this.favPlayers[i].label = player;
+      this.favPlayers[i].value = `${player + i}`;
+
+      // console.log(this.favPlayers[i].label , this.favPlayers[i].value);
     }
   } else if (error) {
     this.error = error;
   }
 }
+get playerOptions() {
+  return this.favPlayers.map(player => {
+      return { label: player.label, value: player.value };
+  });
+}
 
+handleChange(event) {
+  this.selectedPlayer = event.detail.value;
+    // console.log('Selected player:', this.selectedPlayer);
+}
 
-    get options() {
-      return this.favPlayers;
-    }
     
 }
 
