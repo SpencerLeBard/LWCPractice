a52b215b950e0d7b8b73986cc1c98feeaffd99ea
SpencerLeBard <slebard@gmail.com>
2023-04-13 10:52:51 -0600

    Both drop downs showing up

--- force-app/main/default/lwc/customMetadataTypesDropdown/customMetadataTypesDropdown.js
+++ force-app/main/default/lwc/customMetadataTypesDropdown/customMetadataTypesDropdown.js
@@ -1,8 +1,11 @@
 import { LightningElement, track, wire } from 'lwc';
 import getFavoritePlayerPicklistValues from '@salesforce/apex/customSoccerMdtController.getFavoritePlayerPicklistValues';
+import  getFavoriteTeamPicklistValues  from '@salesforce/apex/customSoccerMdtController.getFavoriteTeamPicklistValues';
+
 
 export default class CustomMetadataTypesDropdown extends LightningElement {
     @track favPlayers = [];
+    @track favTeams = [];
     @track data;
     @track error;
 
@@ -13,32 +16,55 @@ getFavPlayer({ data, error }) {
       let player = data[i];
       // console.log({label: player, value: `${player + i}`})
       this.favPlayers.push({label: player, value: `${player + i}`});
-
-      //NOTE this.favPlayer is returning a proxy object and i can't figure out how to drill into the data
-
       this.favPlayers[i].label = player;
       this.favPlayers[i].value = `${player + i}`;
-
       // console.log(this.favPlayers[i].label , this.favPlayers[i].value);
     }
   } else if (error) {
     this.error = error;
   }
 }
+
+@wire(getFavoriteTeamPicklistValues)
+getFavTeam({ data, error }) {
+  if (data) {
+    for(let i = 0; i <= data.length - 1; i++){
+      let team = data[i];
+      // console.log({label: team, value: `${team + i}`})
+      this.favTeams.push({label: team, value: `${team + i}`});
+      this.favTeams[i].label = team;
+      this.favTeams[i].value = `${team + i}`;
+      // console.log(this.favTeams[i].label , this.favTeams[i].value);
+    }
+  } else if (error) {
+    this.error = error;
+  }
+}
+
 get playerOptions() {
   return this.favPlayers.map(player => {
       return { label: player.label, value: player.value };
   });
 }
 
-handleChange(event) {
+handleChangePlayer(event) {
   this.selectedPlayer = event.detail.value;
     // console.log('Selected player:', this.selectedPlayer);
+}
+
+get teamOptions() {
+  return this.favTeams.map(team => {
+      return { label: team.label, value: team.value };
+  });
+}
+
+handleChangeTeam(event) {
+  this.selectedTeam = event.detail.value;
+    // console.log('Selected Team:', this.selectedTeam);
 }
 
     
 }
 
 
-  // import { getFavoriteTeamPicklistValues } from '@salesforce/apex/customSoccerMdtController.getFavoriteTeamPicklistValues';
 
